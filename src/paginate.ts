e8ecce11bc43ea55e4f77a6713515767d5ec2647
Gregor Martynus <gregor@martynus.net>
2019-10-27 15:04:41 -0700

    feat: `.paginate()` and `.paginate.iterator()` methods

---
+++ src/paginate.ts
@@ -0,0 +1,58 @@
+import { Octokit } from "@octokit/core";
+
+import { iterator } from "./iterator";
+import {
+  MapFunction,
+  PaginationResults,
+  RequestParameters,
+  Route
+} from "./types";
+
+export function paginate(
+  octokit: Octokit,
+  route: Route,
+  parameters?: RequestParameters,
+  mapFn?: MapFunction
+) {
+  if (typeof parameters === "function") {
+    mapFn = parameters;
+    parameters = undefined;
+  }
+
+  return gather(
+    octokit,
+    [],
+    iterator(octokit, route, parameters)[
+      Symbol.asyncIterator
+    ]() as AsyncIterableIterator<any>,
+    mapFn
+  );
+}
+
+function gather(
+  octokit: Octokit,
+  results: PaginationResults,
+  iterator: AsyncIterableIterator<any>,
+  mapFn?: MapFunction
+): Promise<PaginationResults> {
+  return iterator.next().then(result => {
+    if (result.done) {
+      return results;
+    }
+
+    let earlyExit = false;
+    function done() {
+      earlyExit = true;
+    }
+
+    results = results.concat(
+      mapFn ? mapFn(result.value, done) : result.value.data
+    );
+
+    if (earlyExit) {
+      return results;
+    }
+
+    return gather(octokit, results, iterator, mapFn);
+  });
+}
