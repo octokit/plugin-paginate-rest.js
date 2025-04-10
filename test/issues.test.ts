996f633db720e5ea6c1721c3da8c470dc9a75c39
wolfy1339 <webmaster@wolfy1339.com>
2025-04-10 00:49:27 -0400

    remove test for deprecated endpoint

--- test/issues.test.ts
+++ test/issues.test.ts
@@ -1,56 +1,8 @@
 import { describe, it, test, expect } from "vitest";
 import fetchMock from "fetch-mock";
 import { Octokit } from "@octokit/core";
-import { restEndpointMethods } from "@octokit/plugin-rest-endpoint-methods";
 
 import { paginateRest } from "../src/index.ts";
-
-describe("https://github.com/octokit/plugin-paginate-rest.js/issues/46", () => {
-  it("octokit.paginate('GET /projects/columns/{column}/cards', { column })", async () => {
-    const mock = fetchMock
-      .createInstance()
-      .get("https://api.github.com/projects/columns/123/cards", {
-        body: [{ id: 123 }],
-      });
-
-    const TestOctokit = Octokit.plugin(paginateRest);
-    const octokit = new TestOctokit({
-      request: {
-        fetch: mock.fetchHandler,
-      },
-    });
-
-    const result = await octokit.paginate(
-      "GET /projects/columns/{column_id}/cards",
-      {
-        column_id: 123,
-      },
-    );
-
-    expect(result[0].id).toEqual(123);
-  });
-
-  it("octokit.paginate(octokit.projects.listCards, { column })", async () => {
-    const mock = fetchMock
-      .createInstance()
-      .get("https://api.github.com/projects/columns/123/cards", {
-        body: [{ id: 123 }],
-      });
-
-    const TestOctokit = Octokit.plugin(paginateRest, restEndpointMethods);
-    const octokit = new TestOctokit({
-      request: {
-        fetch: mock.fetchHandler,
-      },
-    });
-
-    const result = await octokit.paginate(octokit.rest.projects.listCards, {
-      column_id: 123,
-    });
-
-    expect(result[0].id).toEqual(123);
-  });
-});
 
 describe("https://github.com/octokit/plugin-paginate-rest.js/issues/158", () => {
   test("handle 204 response for `GET /repos/{owner}/{repo}/contributors` if repository is empty", async () => {
